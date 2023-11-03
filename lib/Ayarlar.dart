fd80bbc9633e9c9cfc00c1215b570c27da155189
Şeyma Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-11-04 00:19:39 +0300

    Update Ayarlar.dart

--- lib/Ayarlar.dart
+++ lib/Ayarlar.dart
@@ -1,20 +1,111 @@
 import 'package:flutter/material.dart';
 
-class Ayarlar extends StatefulWidget {
-  const Ayarlar({super.key});
-
-  @override
-  State<Ayarlar> createState() => AyarlarState();
+void main() {
+  runApp(Ayarlar());
 }
 
-class AyarlarState extends State<Ayarlar> {
+class Ayarlar extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ayarlar'), 
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFE9DCD0),
+        body: Stack(
+          children: [
+            Positioned(
+              top: 240,
+              left:120,
+              child: ElevatedButton(
+                onPressed: () {
+
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF53715A)),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(100, 100)),
+                ),
+                child: const Text(
+                  "TR",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 240,
+              left: 260,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 157, 91, 43)),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(100, 100)),
+                ),
+                child: const Text(
+                  "ENG",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ),
+            const Positioned(
+              top: 60,
+              left: 130,
+              child: Text(
+                "Ayarlar",
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey,
+                      blurRadius: 2.0,
+                      offset: Offset(2.0, 2.0),
+                    ),
+                  ],
+                  fontSize: 41,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  color: Color.fromARGB(255, 157, 91, 43),
+                  fontFamily: 'RobotoMono',
+                ),
+              ),
+            ),
+            const Positioned(
+              top: 265,
+              right: 310,
+              child: Text(
+                "Dil",
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey,
+                      blurRadius: 1.0,
+                      offset: Offset(2.0, 2.0),
+                    ),
+                  ],
+                  fontSize: 41,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF53715A),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: const Placeholder(), 
     );
   }
 }
+
