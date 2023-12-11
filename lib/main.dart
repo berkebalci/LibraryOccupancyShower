9d72e1327bc6b6f624dba09ea7f5bf5e5122e5a3
berkebalci <berkebalcii38@gmail.com>
2023-12-11 22:24:15 +0300

    Klasörleme hatasi giderildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_application_1/services/pages/anasayfa.dart';
+import 'package:flutter_application_1/pages/anasayfa.dart';
 import 'firebase_options.dart';
 import 'services/firebaseIslemleri.dart';
 //import 'package:flutter_application_1/Ayarlar.dart';
